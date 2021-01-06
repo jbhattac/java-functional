caa63980bd6e1f98bbb63e8663a7678ebde5a67a
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-01-06 08:56:11 -0700

    map

---
+++ code/part2/examples/03_map/Sample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Sample {  
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      
+    numbers.stream()
+      .map(e -> e * 2)
+      .forEach(e -> System.out.println(e));
+  }
+}
+
