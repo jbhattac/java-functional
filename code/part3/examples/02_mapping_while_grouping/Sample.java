92a8e4f269f8c856dba97806afccce75c7fc0371
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-01-06 10:04:52 -0700

    mapping while grouping

---
+++ code/part3/examples/02_mapping_while_grouping/Sample.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
+public class Sample {  
+  public static List<Person> createPeople() {
+    return List.of(
+	    new Person("Sara", 20),
+	    new Person("Sara", 22),
+	    new Person("Bob", 20),
+	    new Person("Paula", 32),
+	    new Person("Paul", 32),
+	    new Person("Jack", 3),
+	    new Person("Jack", 72),
+	    new Person("Jill", 11));
+  }
+
+  public static void main(String[] args) {
+    //get a map of names and ages, but only for those older than 20  
+
+    System.out.println(createPeople()
+      .stream()
+      //.filter(person -> person.getAge() > 20)
+      //.collect(groupingBy(Person::getName)));
+      .collect(groupingBy(Person::getName, 
+        mapping(Person::getAge, toList()))));
+  }
+}
