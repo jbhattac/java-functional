913274e9a715729cf4d1fc0c0929cdeb4185db5a
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-01-06 07:19:02 -0700

    declarative style

---
+++ code/part1/examples/02_declarative_style/Sample.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Sample {  
+  public static void main(String[] args) {
+    List<String> names = Arrays.asList(
+      "Dory", "Gill", "Bruce", "Nemo", "Darla", "Marlin", "Jacques");
+
+
+    //Is Nemo there?
+
+    //imperative style: we tell what to do and also how to do it
+    /*
+    boolean found = false;
+
+    //for(int i = 0; i < names.size(); i++) //oh my  - that is a how
+    for(String name: names) { //that is also a how
+      //if(name == "Nemo") // that is also a how
+      if(name.equals("Nemo")) {// that is also a how
+        found = true;
+	break; //that is also another how
+      }
+    }
+
+    if(found) {
+      System.out.println("Nemo found");
+    } else {
+      System.out.println("Nemo not found");
+    }
+    */
+
+    //Declarative style: tell what to do and *not* how to do it
+    if(names.contains("Nemo")) {
+      System.out.println("Nemo found");
+    } else {
+      System.out.println("Nemo not found");
+    }
+    //This follow the principle of SLAP - Single Level of Abstraction
+    //Principle
+  }
+}
